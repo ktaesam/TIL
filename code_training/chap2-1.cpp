d4cd2b65f69079071b9220f9b64065e09b8404f7
taesam kim <rlaxotka963@gmail.com>
2025-03-17 20:58:26 +0900

    250317 code training chap2-1

---
+++ code_training/chap2-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    string name;
+    cin >> name;
+    cout << "What is your name? " << name << endl;
+    cout << "Hello, " << name << ", nice to meet you!";
+
+    return 0;
+}
