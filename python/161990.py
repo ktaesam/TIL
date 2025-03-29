a825714e1cbde82a49dcfac82164639b66adf474
taesam kim <rlaxotka963@gmail.com>
2025-03-29 22:45:04 +0900

    250329 programmers 161990

---
+++ python/161990.py
@@ -0,0 +1,20 @@
+def solution(wallpaper):
+    answer = []
+    
+    lux = len(wallpaper[0])
+    luy = len(wallpaper)å
+    rdx = 0
+    rdy = 0
+    
+        
+    for y in range(len(wallpaper)):
+        for x in range(len(wallpaper[0])):
+            if wallpaper[y][x] == '#':
+                lux = min(lux, x)
+                luy = min(luy, y)
+                rdx = max(rdx, x + 1)
+                rdy = max(rdy, y + 1)
+    
+    answer = [luy, lux, rdy, rdx]
+    
+    return answer
